98ba279efbe94692b73357ab3d600f57f640b461
Nico Guajardo <nicolasguajardo.96@gmail.com>
2017-04-14 18:46:09 -0300

    clase 1

--- Clase1.py
+++ Clase1.py
@@ -1,1 +1,7 @@
+import Partida
+
 print("hola")
+
+hola=[5]
+
+partida1 = Partida(797,hola)
---
+++ Partida.py
@@ -0,0 +1,7 @@
+class Partida:
+
+    def __init__(self, turno, jugadores):
+        self.turno=turno
+        self.jugadores=jugadores
+
+
