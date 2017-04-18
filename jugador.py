593c8d57967efd88678aac234212eb507e21d43b
NachoRodriguezM <elnachorodriguez@gmail.com>
2017-04-17 23:49:30 -0300

    Parsing

--- jugador.py
+++ jugador.py
@@ -1,14 +1,14 @@
 from pygame import *
 
+
 class Jugador():
     def __init__(self, nombre):
-        self.nombre=nombre
-
-
+        self.nombre = nombre
 
 
 class Ficha(Jugador):
-    ficha1= image.load("Images/ficha-roja.png").convert_alpha()
+    ficha1 = image.load("Images/ficha-roja.png").convert_alpha()
+
     def __init__(self, nombre, ficha):
-        Jugador.__init__(self,nombre)
-        self.ficha1=ficha
+        Jugador.__init__(self, nombre)
+        self.ficha1 = ficha
