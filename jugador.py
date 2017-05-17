af945fe10609e289fa22017e9b34c045106234e7
Berilium25 <razor14250@gmail.com>
2017-05-17 00:23:10 -0300

    Creación clase Partida

--- jugador.py
+++ jugador.py
@@ -2,8 +2,9 @@ from pygame import *
 
 
 class Jugador():
-    def __init__(self, nombre):
+    def __init__(self, nombre, numero):
         self.nombre = nombre
+        self.numero = numero
 
 
 class Ficha(Jugador):
@@ -12,3 +13,24 @@ class Ficha(Jugador):
     def __init__(self, nombre, ficha):
         Jugador.__init__(self, nombre)
         self.ficha1 = ficha
+
+
+class Partida:
+    def __init__(self, nombre, numero, es_corredor):
+        self.nombre = nombre
+        self.numero = numero
+        self.es_corredor = es_corredor
+        self.participan = Jugador()
+        self.tiene = Turno()
+        self.tiene = Dado()
+        self.tiene = Tablero()
+        self.jugadores = []
+
+    def crearJugador(self, avatar):
+        self.avatar = avatar
+        self.participan.Jugador.nombre = self.nombre
+        self.numero = self.participan.Jugador.numero
+        if self.es_corredor==True:
+            return self.participan.Jugador.Corredor
+        else:
+            return self.participan.Jugador.Contador
