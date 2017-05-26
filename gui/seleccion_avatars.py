6d639479c41aeca492d09c77f88ea8cd2ad73a5e
NachoRodriguezM <elnachorodriguez@gmail.com>
2017-05-26 04:21:32 -0300

    Actualizacion de vista seleccion avatar con fondo imagen

--- gui/seleccion_avatars.py
+++ gui/seleccion_avatars.py
@@ -7,12 +7,12 @@ class avatars:
     mouseAction = mousePressed()
 
     def __init__(self):
-        screenSize(800, 400)
-        setBackgroundColour(COLORES.BLANCO)
+        screenSize(800, 420)
+        setBackgroundImage("wall.jpg")
         self.xposCaza = 25
-        self.xposCont = 25
+        self.xposCont = 500
         self.yposCaza = 60
-        self.yposCont = 225
+        self.yposCont = 60
         self.numAvatar = None
         self.sprite1 = makeSprite("avatar1.png")
         self.sprite2 = makeSprite("avatar2.png")
@@ -20,8 +20,9 @@ class avatars:
         self.sprite4 = makeSprite("avatar4.png")
         self.sprite5 = makeSprite("avatar5.png")
         self.lista = [self.sprite1, self.sprite2, self.sprite3, self.sprite4, self.sprite5]
+        drawRect(360,60,1,320,'white')
 
-        self.cazadores = makeLabel('Cazadores:', 36, 25, 5)
+        self.cazadores = makeLabel('Cazadores:', 36, 25, 5,'white')
         showLabel(self.cazadores)
 
         for avatar in range(0, 2):
@@ -29,7 +30,7 @@ class avatars:
             showSprite(self.lista[avatar])
             self.xposCaza += 80
 
-        self.contadores = makeLabel('Contadores:', 36, 25, 170)
+        self.contadores = makeLabel('Contadores:', 36, 500, 5,'white')
         showLabel(self.contadores)
 
         for avatar in range(2, 5):
@@ -38,23 +39,28 @@ class avatars:
             self.xposCont += 80
 
         while True:
-            if Clickeable().elemento_precionado(mousePressed(), self.sprite1.rect[0], self.sprite1.rect[1],
+            if Clickeable().elemento_precionado(mousePressed(),
+                                                self.sprite1.rect[0], self.sprite1.rect[1],
                                                 self.sprite1.rect[2], self.sprite1.rect[3]):
                 self.numAvatar = 0
                 break
-            if Clickeable().elemento_precionado(mousePressed(), self.sprite2.rect[0], self.sprite2.rect[1],
+            if Clickeable().elemento_precionado(mousePressed(),
+                                                self.sprite2.rect[0], self.sprite2.rect[1],
                                                 self.sprite2.rect[2], self.sprite2.rect[3]):
                 self.numAvatar = 1
                 break
-            if Clickeable().elemento_precionado(mousePressed(), self.sprite3.rect[0], self.sprite3.rect[1],
+            if Clickeable().elemento_precionado(mousePressed(),
+                                                self.sprite3.rect[0], self.sprite3.rect[1],
                                                 self.sprite3.rect[2], self.sprite3.rect[3]):
                 self.numAvatar = 2
                 break
-            if Clickeable().elemento_precionado(mousePressed(), self.sprite4.rect[0], self.sprite4.rect[1],
+            if Clickeable().elemento_precionado(mousePressed(),
+                                                self.sprite4.rect[0], self.sprite4.rect[1],
                                                 self.sprite4.rect[2], self.sprite4.rect[3]):
                 self.numAvatar = 3
                 break
-            if Clickeable().elemento_precionado(mousePressed(), self.sprite5.rect[0], self.sprite5.rect[1],
+            if Clickeable().elemento_precionado(mousePressed(),
+                                                self.sprite5.rect[0], self.sprite5.rect[1],
                                                 self.sprite5.rect[2], self.sprite5.rect[3]):
                 self.numAvatar = 4
                 break
