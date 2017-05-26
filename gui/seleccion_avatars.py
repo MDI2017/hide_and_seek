0afb5db9891c9f583cdd6179fde9c08b3ecff745
Ignacio <elnachorodriguez@gmail.com>
2017-05-25 22:05:04 -0300

    Avatares prelimiraes y funcion de seleccion de avatar - Exitosa Realizacion de division de roles de jugadores  - en proceso

--- gui/seleccion_avatars.py
+++ gui/seleccion_avatars.py
@@ -1,5 +1,6 @@
 from pygame_functions import *
 from constantes import *
+from gui.clickeable import Clickeable
 
 
 class avatars:
@@ -16,29 +17,42 @@ class avatars:
         self.sprite5 = makeSprite("avatar5.png")
         self.lista = [self.sprite1,self.sprite2,self.sprite3,self.sprite4,self.sprite5]
 
-        for avatar in range(0,5):
+        self.cazador=makeLabel('Cazador:',58,25,5)
+        showLabel(self.cazador)
+
+        for avatar in range(0,2):
             moveSprite(self.lista[avatar], self.xpos ,15)
             showSprite(self.lista[avatar])
             self.xpos += 80
-            pause(200)
+
+        for avatar in range(0,3):
+            moveSprite(self.lista[avatar], self.xpos ,15)
+            showSprite(self.lista[avatar])
+            self.xpos += 80
+
 
         while True:
-            if spriteClicked(self.sprite1):
-                print('hola')
-                self.numAvatar=0
+            if Clickeable().elemento_precionado(mousePressed(),self.sprite1.rect[0],self.sprite1.rect[1],
+                                                self.sprite1.rect[2],self.sprite1.rect[3]):
+                self.numAvatar = 0
                 break
-            if spriteClicked(self.sprite2):
-                self.numAvatar=1
+            if Clickeable().elemento_precionado(mousePressed(),self.sprite2.rect[0],self.sprite2.rect[1],
+                                                self.sprite2.rect[2],self.sprite2.rect[3]):
+
+                self.numAvatar = 1
                 break
-            if spriteClicked(self.sprite3):
-                self.numAvatar=2
+            if Clickeable().elemento_precionado(mousePressed(),self.sprite3.rect[0],self.sprite3.rect[1],
+                                                self.sprite3.rect[2],self.sprite3.rect[3]):
+                self.numAvatar = 2
                 break
-            if spriteClicked(self.sprite4):
-                self.numAvatar=3
-                pygame.quit()
-            if spriteClicked(self.sprite5):
+            if Clickeable().elemento_precionado(mousePressed(),self.sprite4.rect[0],self.sprite4.rect[1],
+                                                self.sprite4.rect[2],self.sprite4.rect[3]):
+                self.numAvatar = 3
+                break
+            if Clickeable().elemento_precionado(mousePressed(),self.sprite5.rect[0],self.sprite5.rect[1],
+                                                self.sprite5.rect[2],self.sprite5.rect[3]):
                 self.numAvatar=4
-                pygame.quit()
+                break
 
 
 
