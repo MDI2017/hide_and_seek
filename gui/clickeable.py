2f4a739532ff548e8386030f2ee0c1cdf7136e6a
dstocco <daniel.stocco@gmail.com>
2017-05-26 02:47:20 -0300

    La clase Clicleable recibe rects

--- gui/clickeable.py
+++ gui/clickeable.py
@@ -1,11 +1,9 @@
-from .posicionable import Posicionable
+from constantes import RECT
 
 
 class Clickeable:
-
-
     @staticmethod
-    def elemento_precionado (posicion_mouse, posicion_x, posicion_y, alto, ancho):
+    def click_elemento(posicion_mouse, posicion_x, posicion_y, alto, ancho):
         """
         Esta funcion recibe un tupla con un con las coordenadas de el lugar donde el mouse hizo click
         :param posicion:  
@@ -15,6 +13,16 @@ class Clickeable:
         if not posicion_mouse:
             return False
 
-        if posicion_x < posicion_mouse[0] < posicion_x + alto \
-                and posicion_y < posicion_mouse[1] < posicion_y + ancho:
-            return True
+        return Clickeable._clickeado(posicion_mouse, posicion_x, posicion_x + ancho, posicion_y, posicion_y + alto)
+
+    @staticmethod
+    def click_sprite(posicion_mouse, rect_sprite):
+        if not posicion_mouse:
+            return False
+        x_final = rect_sprite[RECT.POS_X] + rect_sprite[RECT.ANCHO]
+        y_final = rect_sprite[RECT.POS_Y] + rect_sprite[RECT.ALTO]
+        return Clickeable._clickeado(posicion_mouse, rect_sprite[RECT.POS_X], x_final, rect_sprite[RECT.POS_Y], y_final)
+
+    @staticmethod
+    def _clickeado(posicion_mouse, x_inicial, x_final, y_inicial, y_final):
+        return x_inicial < posicion_mouse[0] < x_final and y_inicial < posicion_mouse[1] < y_final
