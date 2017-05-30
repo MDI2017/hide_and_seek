94025e08ed4ccd25c41447454695ea148585362f
NachoRodriguezM <elnachorodriguez@gmail.com>
2017-05-30 14:27:23 -0300

    Cambio logica con clickeable en vez de standard de py_functions - no listo todavia

--- gui/seleccion_avatars.py
+++ gui/seleccion_avatars.py
@@ -1,11 +1,12 @@
 from pygame_functions import *
 from gui.clickeable import Clickeable
+from gui.button import Button
 
 
-class avatars:
-    mouseAction = mousePressed()
+class Avatars:
 
     def __init__(self):
+        # Definicion de fondo, atributos a usar y Sprites
         screenSize(800, 420)
         setBackgroundImage("wall.jpg")
         self.xposCaza = 25
@@ -13,47 +14,46 @@ class avatars:
         self.yposCaza = 60
         self.yposCont = 60
         self.numAvatar = None
-        self.sprite1 = makeSprite("avatar1.png")
-        self.sprite2 = makeSprite("avatar2.png")
-        self.sprite3 = makeSprite("avatar3.png")
-        self.sprite4 = makeSprite("avatar4.png")
-        self.sprite5 = makeSprite("avatar5.png")
+        self.sprite1, self.sprite2, self.sprite3, self.sprite4, self.sprite5 = None, None, None, None, None
         self.lista = [self.sprite1, self.sprite2, self.sprite3, self.sprite4, self.sprite5]
-        drawRect(360,60,1,320,'white')
+        drawRect(360, 60, 1, 320, 'white')
 
-        self.cazadores = makeLabel('Cazadores:', 36, 25, 5,'white')
+        # Titulos de categoria Sprite
+        self.cazadores = makeLabel('Cazadores', 36, 25, 5, 'white')
         showLabel(self.cazadores)
 
-        for avatar in range(0, 2):
-            moveSprite(self.lista[avatar], self.xposCaza, self.yposCaza)
-            showSprite(self.lista[avatar])
-            self.xposCaza += 80
-
-        self.contadores = makeLabel('Contadores:', 36, 500, 5,'white')
+        self.contadores = makeLabel('Contadores', 36, 500, 5, 'white')
         showLabel(self.contadores)
 
+        # Dibujado de Sprites
+        for avatar in range(0, 2):
+            self.lista[avatar] = Button(self.xposCaza, self.yposCaza, "avatar1.png")
+            self.xposCaza += 80
+
         for avatar in range(2, 5):
-            moveSprite(self.lista[avatar], self.xposCont, self.yposCont)
-            showSprite(self.lista[avatar])
+            self.lista[avatar] = Button(self.xposCont, self.yposCont, "avatar1.png")
             self.xposCont += 80
 
-        while True:
-            if Clickeable().click_sprite(mousePressed(), self.sprite1.rect):
-                self.numAvatar = 0
-                break
-            if Clickeable().click_sprite(mousePressed(), self.sprite2.rect):
-                self.numAvatar = 1
-                break
-            if Clickeable().click_sprite(mousePressed(), self.sprite3.rect):
-                self.numAvatar = 2
-                break
-            if Clickeable().click_sprite(mousePressed(), self.sprite4.rect):
-                self.numAvatar = 3
-                break
-            if Clickeable().click_sprite(mousePressed(), self.sprite5.rect):
-                self.numAvatar = 4
-                break
+        # Asignacion de funcionalidad clickeable
+        # while True:
+        #     if Clickeable().click_elemento():
+        #         self.numAvatar = 0
+        #         break
+        #     if Clickeable().click_sprite(mousePressed(), self.sprite2.rect):
+        #         self.numAvatar = 1
+        #         break
+        #     if Clickeable().click_sprite(mousePressed(), self.sprite3.rect):
+        #         self.numAvatar = 2
+        #         break
+        #     if Clickeable().click_sprite(mousePressed(), self.sprite4.rect):
+        #         self.numAvatar = 3
+        #         break
+        #     if Clickeable().click_sprite(mousePressed(), self.sprite5.rect):
+        #         self.numAvatar = 4
+        #         break
 
 
-av = avatars()
+av = Avatars()
 print(av.numAvatar)
+print(av.sprite1.rect)
+
