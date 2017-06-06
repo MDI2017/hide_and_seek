1ae5abe376a3de39fbf12ac96248794213dc2a6c
dstocco <daniel.stocco@gmail.com>
2017-06-06 19:58:32 -0300

    Lista lógica de agregar jugadores

--- gui/setting_partida.py
+++ gui/setting_partida.py
@@ -1,21 +1,18 @@
-
 from .button import Button
 from .datos_jugador import GrillaJugador
 import pygame
 from constantes import SEPARACIONES, RECT
 
+
 class SettingPartida:
     def __init__(self):
-        self.jugadores=[]
+        self.jugadores = []
         self.grillas = [GrillaJugador(SEPARACIONES.PADDING_VENTANA_PPAL, SEPARACIONES.PADDING_VENTANA_PPAL),
-                        GrillaJugador(SEPARACIONES.PADDING_VENTANA_PPAL, 80),
-                        # GrillaJugador(100, 260),
-                        # GrillaJugador(100, 340),
-                        ]
+                        GrillaJugador(SEPARACIONES.PADDING_VENTANA_PPAL, 80)]
+
         self.botonAgregarJugador = None
         self.botonCazadorAleatorio = None
         self.botonComenzarPartida = None
-
 
     def mostrar_pantalla_sentting(self):
         pygame.display.set_caption("SETTING PARTIDA")
@@ -55,17 +52,13 @@ class SettingPartida:
                             self.grillas[-1].radioButtonCazador.ocultar()
 
                             self.grillas.pop()
+
+                        if len(self.grillas) < 5 and self.botonAgregarJugador.inactivo:
+                            self.botonAgregarJugador.activar()
+
                         break
                     if self.botonAgregarJugador.click_elemento(mouseAction):
                         self._agregar_grilla()
-
-                        # LO QUE HAY DENTRO DE ESTE IF ES SOLO PARA PROBAR EL boton "botonBorrar".
-                        #     grillaa = GrillaJugador(SEPARACIONES.PADDING_VENTANA_PPAL, 144)
-                        #     self.grillas.append(grillaa)
-                        #     grillaa.dibujar_grilla()
-                        #     grillaa = GrillaJugador(SEPARACIONES.PADDING_VENTANA_PPAL, 208)
-                        #     self.grillas.append(grillaa)
-                        #     grillaa.dibujar_grilla()
                         break
                     if self.botonCazadorAleatorio.click_elemento(mouseAction):
                         break
@@ -82,32 +75,45 @@ class SettingPartida:
         print("aqui llamo a la funcion que abre la lista de avatars con el id: " + str(numero_jugador))
 
     def _agregar_grilla(self):
+
         indice_ultima_grilla = len(self.grillas) - 1
-        pos_y_nueva_grilla = self.grillas[indice_ultima_grilla].altoGrilla + self.grillas[indice_ultima_grilla].posY + SEPARACIONES.SEPARACION
+
+        pos_y_nueva_grilla = self.grillas[indice_ultima_grilla].posY + SEPARACIONES.SEPARACION + 56
+
         grilla = GrillaJugador(SEPARACIONES.PADDING_VENTANA_PPAL, pos_y_nueva_grilla)
+
         self.grillas.append(grilla)
         grilla.dibujar_grilla()
 
+        if len(self.grillas) == 5:
+            self.botonAgregarJugador.desactivar()
+
     def _dibujar_botones_accion(self):
 
-        self.botonAgregarJugador = Button(300, SEPARACIONES.PADDING_VENTANA_PPAL, "boton_agregar_jugador_habilitado.png")
+        self.botonAgregarJugador = Button(300, SEPARACIONES.PADDING_VENTANA_PPAL,
+                                          "boton_agregar_jugador_habilitado.png")
         self.botonAgregarJugador.agregar_imagen("boton_agregar_jugador_presionado.png")
         self.botonAgregarJugador.agregar_imagen("boton_agregar_jugador_desactivado.png")
         self.botonAgregarJugador.dibujar()
 
-        posicion_x_botones = pygame.display.Info().current_w - self.botonAgregarJugador.rect[RECT.ANCHO] - SEPARACIONES.PADDING_VENTANA_PPAL
+        posicion_x_botones = pygame.display.Info().current_w - self.botonAgregarJugador.rect[
+            RECT.ANCHO] - SEPARACIONES.PADDING_VENTANA_PPAL
         self.botonAgregarJugador.mover(pos_x=posicion_x_botones)
 
-        posicion_y_cazador_aleatorio = self.botonAgregarJugador.rect[RECT.POS_Y] + self.botonAgregarJugador.rect[RECT.ALTO] + SEPARACIONES.SEPARACION
+        posicion_y_cazador_aleatorio = self.botonAgregarJugador.rect[RECT.POS_Y] + self.botonAgregarJugador.rect[
+            RECT.ALTO] + SEPARACIONES.SEPARACION
 
-        self.botonCazadorAleatorio = Button(posicion_x_botones, posicion_y_cazador_aleatorio, "boton_cazador_aleatorio_habilitado.png")
+        self.botonCazadorAleatorio = Button(posicion_x_botones, posicion_y_cazador_aleatorio,
+                                            "boton_cazador_aleatorio_habilitado.png")
         self.botonCazadorAleatorio.agregar_imagen("boton_cazador_aleatorio_presionado.png")
         self.botonCazadorAleatorio.agregar_imagen("boton_cazador_aleatorio_desactivado.png")
         self.botonCazadorAleatorio.dibujar()
 
-        posicion_y_iniciar_partida = self.botonCazadorAleatorio.rect[RECT.POS_Y] + self.botonAgregarJugador.rect[RECT.ALTO] + SEPARACIONES.SEPARACION
+        posicion_y_iniciar_partida = self.botonCazadorAleatorio.rect[RECT.POS_Y] + self.botonAgregarJugador.rect[
+            RECT.ALTO] + SEPARACIONES.SEPARACION
 
-        self.botonComenzarPartida = Button(posicion_x_botones, posicion_y_iniciar_partida, "boton_iniciar_partida_habilitado.png")
+        self.botonComenzarPartida = Button(posicion_x_botones, posicion_y_iniciar_partida,
+                                           "boton_iniciar_partida_habilitado.png")
         self.botonComenzarPartida.agregar_imagen("boton_iniciar_partida_presionado.png")
         self.botonComenzarPartida.agregar_imagen("boton_iniciar_partida_desactivado.png")
         self.botonComenzarPartida.dibujar()
