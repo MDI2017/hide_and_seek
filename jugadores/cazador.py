dca2b73bfc3f369fde54ea271b3bb0adfbb266b3
NachoRodriguezM <elnachorodriguez@gmail.com>
2017-06-16 01:11:52 -0300

    Si no se elige ningun avatar se toma por defecto 'no_avatar.png'

--- jugadores/cazador.py
+++ jugadores/cazador.py
@@ -5,6 +5,8 @@ from .jugador import Jugador
 class Cazador(Jugador):
     def __init__(self, nombre, avatar):
         super().__init__(nombre, avatar)
+        if self.avatar is None:
+            self.avatar = "no_avatar.png"
         self.crear_ficha([4, 11])
 
     def crear_ficha(self, casillero):
--- jugadores/corredor.py
+++ jugadores/corredor.py
@@ -5,6 +5,8 @@ from .jugador import Jugador
 class Corredor(Jugador):
     def __init__(self, nombre, avatar):
         super().__init__(nombre, avatar)
+        if self.avatar is None:
+            self.avatar = "no_avatar.png"
 
     def crear_ficha(self, numero_jugador):
         if numero_jugador >= 2:
