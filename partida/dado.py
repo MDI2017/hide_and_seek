e3c2c9ac4f9efcfce0aed956a4f630a29163a1a4
AdrianoM96 <adrym09@gmail.com>
2017-06-16 13:46:35 -0300

    Agregada la funcion tirarDado

---
+++ partida/dado.py
@@ -0,0 +1,33 @@
+import random
+from pygame_functions import *
+
+class Dado():
+    def __init__(self):
+        self.dadoImg = makeSprite("images/dice1.png")
+        addSpriteImage(self.dadoImg, "images/dice2.png")
+        addSpriteImage(self.dadoImg, "images/dice3.png")
+        addSpriteImage(self.dadoImg, "images/dice4.png")
+        addSpriteImage(self.dadoImg, "images/dice5.png")
+        addSpriteImage(self.dadoImg, "images/dice6.png")
+        moveSprite(self.dadoImg, 670, 100, True)
+        showSprite(self.dadoImg)
+        etiqueta = makeLabel("Movimientos: ", 20, 610, 20, "black")
+        showLabel(etiqueta)
+
+
+
+    def tirarDado(self):
+        i = 0
+        movimientos = 0
+        while i < 6:
+            changeSpriteImage(self.dadoImg, i)
+            i += 1
+            pause(100)
+            if i == 6:
+                i = 0
+            if spriteClicked(self.dadoImg):
+                movimientos = random.randint(1, 6)
+                changeSpriteImage(self.dadoImg, movimientos )
+                break
+        return movimientos
+
