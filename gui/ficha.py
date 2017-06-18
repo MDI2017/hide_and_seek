56268f0b5a57e3ad26d39e52e6da1dedd8eb2dae
Berilium25 <razor14250@gmail.com>
2017-06-18 12:41:42 -0300

    Creación clase Partida

--- gui/ficha.py
+++ gui/ficha.py
@@ -2,7 +2,6 @@ import pygame
 from gui.dibujable import Dibujable
 from constantes import CASILLAS
 
-
 class Ficha(Dibujable):
     def __init__(self, casillero, nombre_archivo, ancho=60, alto=60, dibujado=False):
         super().__init__(0, 0, nombre_archivo=nombre_archivo, ancho=ancho, alto=alto, dibujado=dibujado)
@@ -11,7 +10,7 @@ class Ficha(Dibujable):
         self.posicionY = CASILLAS.LADO * self.casillero[1]
 
     def mover_ficha(self, direccion):
-        print(self.casillero)
+
         if direccion == pygame.K_UP:
             self.casillero[1] -= 1
         elif direccion == pygame.K_DOWN:
@@ -21,6 +20,7 @@ class Ficha(Dibujable):
         elif direccion == pygame.K_LEFT:
             self.casillero[0] -= 1
         self.mover(CASILLAS.LADO * self.casillero[0], CASILLAS.LADO * self.casillero[1])
+        print(self.casillero)
 
     def _al_mover(self):
         pass
