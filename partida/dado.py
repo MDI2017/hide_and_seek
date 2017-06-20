c2b210397af628e78c59808b0e554da8e409294a
dstocco <daniel.stocco@gmail.com>
2017-06-20 10:25:56 -0300

    refactor dado

--- partida/dado.py
+++ partida/dado.py
@@ -1,20 +1,17 @@
 import random
 from pygame_functions import *
+from gui.dibujable import Dibujable
+from gui.clickeable import Clickeable
 
-class Dado():
-    def __init__(self):
-        self.dadoImg = makeSprite("images/dice1.png")
-        addSpriteImage(self.dadoImg, "images/dice2.png")
-        addSpriteImage(self.dadoImg, "images/dice3.png")
-        addSpriteImage(self.dadoImg, "images/dice4.png")
-        addSpriteImage(self.dadoImg, "images/dice5.png")
-        addSpriteImage(self.dadoImg, "images/dice6.png")
-        moveSprite(self.dadoImg, 670, 100, True)
-        showSprite(self.dadoImg)
+
+class Dado(Clickeable, Dibujable):
+
+    def __init__(self, posicion_x=670, posicion_y=100, nombre_archivo="dice1.png",
+                 ancho=None, alto=None, dibujado=False):
+        super().__init__(posicion_x, posicion_y, nombre_archivo)
+
         self.etiqueta = makeLabel("Movimientos: ", 20, 610, 20, "black")
         showLabel(self.etiqueta)
-
-
 
     def tirarDado(self):
         pause(100)
