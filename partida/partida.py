5c83f07cdb0fd63800fba09c9a19a11b472881c2
Ignacio Rodriguez <elnachorodriguez@gmail.com>
2017-06-20 17:26:08 -0300

    implemetada regla de primer turno doble para contadores

--- partida/partida.py
+++ partida/partida.py
@@ -23,6 +23,7 @@ class Partida():
         self.boton_atras.agregar_imagen("boton_iniciar_partida_presionado.png")
         self.boton_atras.agregar_imagen("boton_iniciar_partida_desactivado.png")
         self.turno = 0
+        self.primer_turno = True
         self.dado = None
         self.info = None
         self.movimientos = 0
@@ -144,12 +145,22 @@ class Partida():
         else:
             self.turno += 1
 
-        self.info.jugador_actual(self.turno)
-        self.movimientos = Dado().tirarDado()  # ACA TIRA ERROR
+        if self.primer_turno is True and self.turno is len(self.corredores):
+            self.info.jugador_actual(0)
+        else:
+            self.info.jugador_actual(self.turno)
+
+        self.movimientos = Dado().tirarDado()
         self.info.movim_restantes(self.movimientos)
 
         if self.turno < len(self.corredores):
             return str(self.turno)
         else:
-            self.turno = "cazador"
-            return self.turno
+            if self.primer_turno is True:
+                print(self.turno)
+                self.turno = 0
+                self.primer_turno = False
+                return str(self.turno)
+            else:
+                self.turno = 'cazador'
+                return self.turno
