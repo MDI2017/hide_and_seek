eeb08d0780b07a920f00271869a180a46376770b
dstocco <daniel.stocco@gmail.com>
2017-06-21 03:00:22 -0300

    Lógica avistaje ok

--- partida/partida.py
+++ partida/partida.py
@@ -11,7 +11,7 @@ from pygame_functions import *
 from constantes import DIVISIONES, ZONAS, CASILLAS
 
 
-class Partida():
+class Partida:
     """
     Clase encargada de dibujar el tablero, iniciar la partida, crear los jugadores (cazador y corredor/es), del 
     movimiento de las fichas durante cada turno y del cambio de turno.
@@ -256,14 +256,13 @@ class Partida():
 
     def _fin_turno(self):
 
-        # if self.turno == 'cazador':
-        if True:
+        # if True:
+        if self.turno == 'cazador':
             casillero_cazador = self.cazador.ficha.casillero
 
             # Iterecion desde la pocición del cazador hacia abajo
             for fila in range(casillero_cazador[CASILLAS.FILA] + 1, 12):
                 casillero = self.tablero.casilleros[casillero_cazador[CASILLAS.COLUMNA]][fila]
-                print(casillero.paredes)
                 if casillero.esta_ocupado:
                     print('avistaje')
                     print(str(casillero_cazador[CASILLAS.COLUMNA]), str(fila))
@@ -277,23 +276,67 @@ class Partida():
             # Iterecion desde la pocición del cazador hacia arriba
             for i in range(0, casillero_cazador[CASILLAS.FILA]):
                 indice_filas = casillero_cazador[CASILLAS.FILA] - 1 - i
-                print(self.tablero.casilleros[casillero_cazador[CASILLAS.COLUMNA]][indice_filas].paredes)
+
+                casillero = self.tablero.casilleros[casillero_cazador[CASILLAS.COLUMNA]][indice_filas]
+
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(casillero_cazador[CASILLAS.COLUMNA]), str(indice_filas))
+                    break
+
+                if casillero.paredes[DIVISIONES.SUPERIOR]:
+                    print('pared')
+                    print(str(casillero_cazador[CASILLAS.COLUMNA]), str(indice_filas))
+                    break
 
             # Iterecion desde la pocición del cazador hacia derecha
             for columna in range(casillero_cazador[CASILLAS.COLUMNA] + 1, 10):
-                print(self.tablero.casilleros[columna][casillero_cazador[CASILLAS.FILA]].paredes)
+                casillero = self.tablero.casilleros[columna][casillero_cazador[CASILLAS.FILA]]
+
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(casillero_cazador[columna]), str(casillero_cazador[CASILLAS.FILA]))
+                    break
+
+                if casillero.paredes[DIVISIONES.DERECHA]:
+                    print('pared')
+                    print(str(casillero_cazador[columna]), str(casillero_cazador[CASILLAS.FILA]))
+                    break
 
             # Iterecion desde la pocición del cazador hacia izquierda
             for i in range(0, casillero_cazador[CASILLAS.COLUMNA]):
                 indice_columnas = casillero_cazador[CASILLAS.COLUMNA] - 1 - i
-                print(self.tablero.casilleros[indice_columnas][casillero_cazador[CASILLAS.FILA]].paredes)
+                casillero = self.tablero.casilleros[indice_columnas][casillero_cazador[CASILLAS.FILA]]
+
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(casillero_cazador[indice_columnas]), str(casillero_cazador[CASILLAS.FILA]))
+                    break
+
+                if casillero.paredes[DIVISIONES.IZQUIERDA]:
+                    print('pared')
+                    print(str(casillero_cazador[indice_columnas]), str(casillero_cazador[CASILLAS.FILA]))
+                    break
+
+
 
             # Iterecion diagonal inferior derecha desde la pocición del cazador
             columna = casillero_cazador[CASILLAS.COLUMNA] + 1
             for fila in range(casillero_cazador[CASILLAS.FILA] + 1, 12):
                 if columna > 9:
                     break
-                print(self.tablero.casilleros[columna][fila].paredes)
+
+                casillero = self.tablero.casilleros[columna][fila]
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(columna), str(fila))
+                    break
+
+                if (casillero.paredes[DIVISIONES.SUPERIOR] and casillero.paredes[DIVISIONES.IZQUIERDA]) \
+                        or (casillero.paredes[DIVISIONES.INFERIOR] and casillero.paredes[DIVISIONES.DERECHA]):
+                    print('pared')
+                    print(str(columna), str(casillero_cazador[fila]))
+                    break
                 columna += 1
 
             # Iterecion diagonal inferior izquierda desde la pocición del cazador
@@ -301,7 +344,18 @@ class Partida():
             for fila in range(casillero_cazador[CASILLAS.FILA] + 1, 12):
                 if columna < 0:
                     break
-                print(self.tablero.casilleros[columna][fila].paredes)
+
+                casillero = self.tablero.casilleros[columna][fila]
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(columna), str(fila))
+                    break
+
+                if (casillero.paredes[DIVISIONES.SUPERIOR] and casillero.paredes[DIVISIONES.DERECHA]) \
+                        or (casillero.paredes[DIVISIONES.INFERIOR] and casillero.paredes[DIVISIONES.IZQUIERDA]):
+                    print('pared')
+                    print(str(columna), str(fila))
+                    break
                 columna -= 1
 
             # Iterecion diagonal superior derecha desde la pocición del cazador
@@ -310,14 +364,36 @@ class Partida():
                 if columna > 9:
                     break
                 indice_filas = casillero_cazador[CASILLAS.FILA] - 1 - i
-                print(self.tablero.casilleros[columna][indice_filas].paredes)
+
+                casillero = self.tablero.casilleros[columna][indice_filas]
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(columna), str(indice_filas))
+                    break
+
+                if (casillero.paredes[DIVISIONES.INFERIOR] and casillero.paredes[DIVISIONES.IZQUIERDA]) or \
+                        (casillero.paredes[DIVISIONES.SUPERIOR] and casillero.paredes[DIVISIONES.DERECHA]):
+                    print('pared')
+                    print(str(columna), str(indice_filas))
+                    break
                 columna += 1
 
             # Iterecion diagonal superior izquierda desde la pocición del cazador
             columna = casillero_cazador[CASILLAS.COLUMNA] - 1
             for i in range(0, casillero_cazador[CASILLAS.FILA]):
-                if columna > 9:
+                if columna < 0:
                     break
                 indice_filas = casillero_cazador[CASILLAS.FILA] - 1 - i
-                print(self.tablero.casilleros[columna][indice_filas].paredes)
+
+                casillero = self.tablero.casilleros[columna][indice_filas]
+                if casillero.esta_ocupado:
+                    print('avistaje')
+                    print(str(columna), str(indice_filas))
+                    break
+
+                if (casillero.paredes[DIVISIONES.INFERIOR] and casillero.paredes[DIVISIONES.DERECHA]) \
+                        or (casillero.paredes[DIVISIONES.SUPERIOR] and casillero.paredes[DIVISIONES.IZQUIERDA]):
+                    print('pared')
+                    print(str(columna), str(indice_filas))
+                    break
                 columna -= 1
